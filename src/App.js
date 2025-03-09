f4a95641660acaf468f0243b8243f1dc30b3be1f
inam <inamassar26@gmail.com>
2025-03-09 22:03:46 +0500

    Intro Section Completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Intro from "./components/intro";
+import "./app.scss";
+
+const App = () => {
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
+};
+
+export default App;
